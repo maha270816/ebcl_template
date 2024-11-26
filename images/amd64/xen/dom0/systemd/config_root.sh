c1797e073d8927ea431c93d36dece4e052d6abf0
Marko Hakkarainen <marko.hakkarainen@elektrobit.com>
2024-11-26 21:06:34 +0200

    rootfs: add fstab

--- images/amd64/xen/dom0/systemd/config_root.sh
+++ images/amd64/xen/dom0/systemd/config_root.sh
@@ -2,3 +2,8 @@
 
 # Link systemd as init
 ln -s /usr/lib/systemd/systemd /sbin/init
+
+# Setup fstab
+mkdir /efi
+echo "/dev/vda1 /efi vfat defaults 0 2" | tee /etc/fstab
+echo "/dev/vda2 / ext4 defaults,errors=remount-ro 0 1" | tee -a /etc/fstab

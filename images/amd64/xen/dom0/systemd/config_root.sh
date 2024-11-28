00515dbe70abb40b204383150bc4fc9f8185d295
Marko Hakkarainen <marko.hakkarainen@elektrobit.com>
2024-11-28 11:56:13 +0200

    dom0: make sure filesystem is mounted as rw

--- images/amd64/xen/dom0/systemd/config_root.sh
+++ images/amd64/xen/dom0/systemd/config_root.sh
@@ -6,4 +6,4 @@ ln -s /usr/lib/systemd/systemd /sbin/init
 # Setup fstab
 mkdir /efi
 echo "/dev/vda1 /efi vfat defaults 0 2" | tee /etc/fstab
-echo "/dev/vda2 / ext4 defaults,errors=remount-ro 0 1" | tee -a /etc/fstab
+echo "/dev/vda2 / ext4 defaults,rw,errors=remount-ro 0 1" | tee -a /etc/fstab
